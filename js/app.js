92f91ae064442eddb3ac0db753fc721020c70cea
JoseirCavassani <joseir.cavassani@hotmail.com>
2022-07-29 12:40:01 +0100

    add responsive menu for small screens

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// responsive menu
+var hamburger = document.querySelector(".hamburger");
+//on click
+hamburger.addEventListener("click", function () {
+  //toggle class "is-actice"
+  hamburger.classList.toggle("is-active");
+  // do something alese, like open/close
+});
+
+/* Toggle between adding and removing the "responsive" class to nav-bar when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myNav-bar");
+  if (x.className === "nav-bar") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-bar";
+  }
+}
